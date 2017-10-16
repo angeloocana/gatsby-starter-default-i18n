0113a09e5af55f378914d75d46901f45b756c489
Angelo Ocana <angeloocana@gmail.com>
2017-10-16 15:31:24 -0400

    Add gatsby-plugin-i18n

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: 'gatsby-plugin-i18n',
+      options: {
+        langKeyForNull: 'any',
+        langKeyDefault: 'en',
+        useLangKeyLayout: false
+      }
+    }],
 }
